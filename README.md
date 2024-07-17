911d758e0809a2d584c755151c275c7bbbd7aac2
2020147542 <tmfal5023@yonsei.ac.kr>
2024-07-18 00:25:19 +0900

    [style] spotless에 의한 formatting

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## 1. 로컬 MySQL 데이터베이스 설정
 - 1단계: MySQL 설치
-- 2단계: MySQL 서비스 시작 
-  - [local-db.env]() 파일의 환경변수와 동일하게 설정하기
-  - env 폴더는 .gitignore에 추가할 예정
+- 2단계: MySQL 서비스 시작
+- [local-db.env]() 파일의 환경변수와 동일하게 설정하기
+- env 폴더는 .gitignore에 추가할 예정
 - 3단계: MySQL 데이터베이스 생성
-  - ```mysql -u root -p```
-  - ```CREATE DATABASE book_moa;```
+- ```mysql -u root -p```
+- ```CREATE DATABASE book_moa;```
--- src/main/java/com/umc/server/ServerApplication.java
+++ src/main/java/com/umc/server/ServerApplication.java
@@ -3,11 +3,11 @@ package com.umc.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 }
