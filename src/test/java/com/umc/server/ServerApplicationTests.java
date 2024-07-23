22b30bde279f6e47207a152488d1fdb97eecd647
2020147542 <tmfal5023@yonsei.ac.kr>
2024-07-24 00:38:56 +0900

    [fix] springboottest 어노테이션 제거

--- src/test/java/com/umc/server/ServerApplicationTests.java
+++ src/test/java/com/umc/server/ServerApplicationTests.java
@@ -1,11 +1,9 @@
 package com.umc.server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
 class ServerApplicationTests {
 
     @Test
