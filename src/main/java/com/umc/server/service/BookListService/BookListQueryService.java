8f51fe098d2c28acc9c429b69edf6b1a03b1df90
Dayun Lee <wohd7877@naver.com>
2024-07-28 00:09:36 +0900

    [refactor] bookListService 형식 변경

--- src/main/java/com/umc/server/service/BookListService/BookListQueryService.java
+++
@@ -1,8 +0,0 @@
-package com.umc.server.service.BookListService;
-
-import com.umc.server.domain.BookList;
-import java.util.Optional;
-
-public interface BookListQueryService {
-    Optional<BookList> getBookList(Long id);
-}
