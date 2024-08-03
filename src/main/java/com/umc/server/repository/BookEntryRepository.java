51d1c33a896b69b24df949d5ee98b60f704a155c
Dayun Lee <110006845+ldayun@users.noreply.github.com>
2024-08-04 01:10:13 +0900

    [feature] 책리스트 검색 (#66)

--- src/main/java/com/umc/server/repository/BookEntryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.umc.server.repository;
-
-import com.umc.server.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookEntryRepository extends JpaRepository<Book, Long> {}
