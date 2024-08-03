2c394d6d761b833cd01f881a364a7cd872616347
dasol729 <97594634+dasol729@users.noreply.github.com>
2024-08-04 04:39:21 +0900

    [feature] 독서모임 생성,조회,수정,삭제 구현 #40

--- src/main/java/com/umc/server/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/umc/server/apiPayload/code/status/ErrorStatus.java
@@ -20,7 +20,23 @@ public enum ErrorStatus implements BaseErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", false, "사용자가 없습니다."),
 
     BOOKLIST_NOT_FOUND(HttpStatus.BAD_REQUEST, "BOOKLIST4001", false, "책리스트가 없습니다."),
-    BOOKLIST_INVALID_STATUS(HttpStatus.BAD_REQUEST, "BOOKLIST4002", false, "STATUS형식이 올바르지 않습니다.");
+    BOOKLIST_INVALID_STATUS(HttpStatus.BAD_REQUEST, "BOOKLIST4002", false, "STATUS형식이 올바르지 않습니다."),
+
+    // 독서 모임
+    CLUB_NOT_FOUND(HttpStatus.BAD_REQUEST, "CLUB4001", false, "독서 모임이 없습니다."),
+    CLUB_NAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "CLUB4002", false, "이미 존재하는 독서 모임 이름입니다."),
+
+    // 독서 모임원
+    CLUB_NOT_JOINED(HttpStatus.BAD_REQUEST, "CLUBMEMBER4001", false, "가입된 독서 모임이 없는 사용자입니다."),
+    CLUB_ALREADY_JOINED(
+            HttpStatus.BAD_REQUEST, "CLUBMEMBER4002", false, "이미 가입된 독서 모임이 있는 사용자입니다."),
+    CLUB_LEADER_REQUIRED(
+            HttpStatus.BAD_REQUEST,
+            "CLUBMEMBER4003",
+            false,
+            "권한이 불충분합니다. 독서 모임 리더의 권한이 필요한 동작입니다."),
+    CLUB_MEMBER_REQUIRED(
+            HttpStatus.BAD_REQUEST, "CLUBMEMBER4004", false, "권한이 불충분합니다. 가입된 독서 모임이 아닙니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
---
+++ src/main/java/com/umc/server/converter/ClubConverter.java
@@ -0,0 +1,63 @@
+package com.umc.server.converter;
+
+import com.umc.server.domain.Club;
+import com.umc.server.web.dto.ClubRequestDTO;
+import com.umc.server.web.dto.ClubResponseDTO;
+import java.util.Optional;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ClubConverter {
+    public static Club toClub(ClubRequestDTO.ClubCreateRequestDTO request, String code) {
+        return Club.builder()
+                .name(request.getName())
+                .intro(request.getIntro())
+                .notice(request.getNotice())
+                .code(code)
+                .password(request.getPassword())
+                .build();
+    }
+
+    public static ClubResponseDTO.ClubCreateResponseDTO toClubCreateResponseDTO(Club club) {
+        return ClubResponseDTO.ClubCreateResponseDTO.builder().clubId(club.getId()).build();
+    }
+
+    public static ClubResponseDTO.MyClubResponseDTO toMyClubResponseDTO(Optional<Club> club) {
+        if (club.isPresent()) {
+            return ClubResponseDTO.MyClubResponseDTO.builder()
+                    .clubId(club.get().getId())
+                    .name(club.get().getName())
+                    .intro(club.get().getIntro())
+                    .build();
+        } else {
+            return new ClubResponseDTO.MyClubResponseDTO();
+        }
+    }
+
+    public static ClubResponseDTO.ClubDetailResponseDTO toClubDetailResponseDTO(Club club) {
+        return ClubResponseDTO.ClubDetailResponseDTO.builder()
+                .clubId(club.getId())
+                .name(club.getName())
+                .intro(club.getIntro())
+                .notice(club.getNotice())
+                .code(club.getCode())
+                .password(club.getPassword())
+                .createAt(club.getCreatedAt())
+                .updateAt(club.getUpdatedAt())
+                .memberCount(club.getClubMemberList().size())
+                .build();
+    }
+
+    public static ClubResponseDTO.ClubUpdateResponseDTO toClubUpdateResponseDTO(Club club) {
+        return ClubResponseDTO.ClubUpdateResponseDTO.builder()
+                .clubId(club.getId())
+                .name(club.getName())
+                .intro(club.getIntro())
+                .notice(club.getNotice())
+                .code(club.getCode())
+                .password(club.getPassword())
+                .createAt(club.getCreatedAt())
+                .updateAt(club.getUpdatedAt())
+                .build();
+    }
+}
