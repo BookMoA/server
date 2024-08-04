b26bd96515052d0aa392105a357a68cfa86552e6
Kiara <80676180+2020147542@users.noreply.github.com>
2024-08-04 12:03:08 +0900

    [chore] 지정된 경우에만 로그인 요청하도록 변경 - 임시

--- src/main/java/com/umc/server/util/JwtAuthFilter.java
+++ src/main/java/com/umc/server/util/JwtAuthFilter.java
@@ -74,21 +74,22 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         return Optional.empty();
     }
 
-    // @Override
-    // protected boolean shouldNotFilter(HttpServletRequest request) {
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
 
-    //     // request 에서 요청 path 추출
-    //     String path = request.getServletPath();
+        // request 에서 요청 path 추출
+        String path = request.getServletPath();
 
-    //     // filter 에서 제외한 url 목록
-    //     String[] excludedPaths = {"/users/auth/", "/swagger-ui/"};
+        // filter 에서 제외한 url 목록
+        // String[] excludedPaths = {"/users/auth/", "/swagger-ui/"};
+        String[] excludedPaths = {"/users/auth/"};
 
-    //     for (String excludedPath : excludedPaths) {
-    //         if (path.startsWith(excludedPath)) {
-    //             return true;
-    //         }
-    //     }
+        for (String excludedPath : excludedPaths) {
+            if (path.startsWith(excludedPath)) {
+                return false;
+            }
+        }
 
-    //     return false;
-    // }
+        return true;
+    }
 }
