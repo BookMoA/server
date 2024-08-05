ddf98d16936e63bb5a145860a1e3f315fd40bcb9
Dayun Lee <wohd7877@naver.com>
2024-08-05 22:02:37 +0900

    [feature] 에러 추가

--- src/main/java/com/umc/server/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/umc/server/apiPayload/code/status/ErrorStatus.java
@@ -20,6 +20,7 @@ public enum ErrorStatus implements BaseErrorCode {
     // 멤버 관려 에러
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", false, "사용자가 없습니다."),
 
+    // 책리스트 관련 에러
     BOOKLIST_NOT_FOUND(HttpStatus.BAD_REQUEST, "BOOKLIST4001", false, "책리스트가 없습니다."),
     BOOKLIST_INVALID_STATUS(HttpStatus.BAD_REQUEST, "BOOKLIST4002", false, "STATUS형식이 올바르지 않습니다."),
     BOOKLIST_BOOK_ALREADY_EXISTS(HttpStatus.NOT_FOUND, "BOOKLIST4003", false, "이미 추가된 책입니다."),
@@ -27,7 +28,11 @@ public enum ErrorStatus implements BaseErrorCode {
 
     BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK4001", false, "없는 책입니다."),
 
-    INVALID_PAGE(HttpStatus.NOT_FOUND, "PAGE4001", false, "잘못된 페이지입니다.");
+    INVALID_PAGE(HttpStatus.NOT_FOUND, "PAGE4001", false, "잘못된 페이지입니다."),
+
+    SEARCH_BOOKLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "SEARCH4001", false, "없는 책리스트입니다."),
+    SEARCH_BOOKMEMO_NOT_FOUND(HttpStatus.NOT_FOUND, "SEARCH4002", false, "없는 메모입니다."),
+    SEARCH_INVALID_SORT(HttpStatus.NOT_FOUND, "SEARCH4003", false, "SORTBY형식이 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/com/umc/server/service/BookListService/BookListServiceImpl.java
+++ src/main/java/com/umc/server/service/BookListService/BookListServiceImpl.java
@@ -1,7 +1,7 @@
 package com.umc.server.service.BookListService;
 
 import com.umc.server.apiPayload.code.status.ErrorStatus;
-import com.umc.server.apiPayload.exception.GeneralException;
+import com.umc.server.apiPayload.exception.handler.BookListHandler;
 import com.umc.server.converter.BookListConverter;
 import com.umc.server.domain.Book;
 import com.umc.server.domain.BookList;
@@ -38,7 +38,7 @@ public class BookListServiceImpl implements BookListService {
         Member member =
                 memberRepository
                         .findById(1L)
-                        .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_NOT_FOUND));
+                        .orElseThrow(() -> new BookListHandler(ErrorStatus.MEMBER_NOT_FOUND));
 
         // DTO를 엔티티로 변환
         BookList bookList = BookListConverter.toBookList(request, member);
@@ -54,14 +54,14 @@ public class BookListServiceImpl implements BookListService {
         BookList bookList =
                 bookListRepository
                         .findById(bookListId)
-                        .orElseThrow(() -> new GeneralException(ErrorStatus.BOOKLIST_NOT_FOUND));
+                        .orElseThrow(() -> new BookListHandler(ErrorStatus.BOOKLIST_NOT_FOUND));
 
         // 2. 상태 값 변환
         ListStatus listStatus;
         try {
             listStatus = ListStatus.valueOf(request.getStatus());
         } catch (IllegalArgumentException e) {
-            throw new GeneralException(ErrorStatus.BOOKLIST_INVALID_STATUS); // status값이 올바르지 않으면 오류
+            throw new BookListHandler(ErrorStatus.BOOKLIST_INVALID_STATUS); // status값이 올바르지 않으면 오류
         }
 
         // 3. 책 리스트 정보 업데이트
@@ -78,7 +78,7 @@ public class BookListServiceImpl implements BookListService {
             Book book =
                     bookRepository
                             .findById(entryDTO.getId())
-                            .orElseThrow(() -> new GeneralException(ErrorStatus.BOOK_NOT_FOUND));
+                            .orElseThrow(() -> new BookListHandler(ErrorStatus.BOOK_NOT_FOUND));
 
             BookListEntry newEntry =
                     BookListEntry.builder()
@@ -108,9 +108,7 @@ public class BookListServiceImpl implements BookListService {
                         bookListRepository
                                 .findById(id)
                                 .orElseThrow(
-                                        () ->
-                                                new GeneralException(
-                                                        ErrorStatus.BOOKLIST_NOT_FOUND)));
+                                        () -> new BookListHandler(ErrorStatus.BOOKLIST_NOT_FOUND)));
         return bookList;
     }
 
@@ -120,7 +118,7 @@ public class BookListServiceImpl implements BookListService {
         BookList bookList =
                 bookListRepository
                         .findById(bookListId)
-                        .orElseThrow(() -> new GeneralException(ErrorStatus.BOOKLIST_NOT_FOUND));
+                        .orElseThrow(() -> new BookListHandler(ErrorStatus.BOOKLIST_NOT_FOUND));
         bookListRepository.delete(bookList);
     }
 
@@ -128,7 +126,7 @@ public class BookListServiceImpl implements BookListService {
     public List<BookListResponseDTO.LibraryBookListDTO> getLibraryBookList(Integer page) {
         // PageRequest를 생성하여 페이지네이션 적용
         Page<BookList> bookLists =
-                bookListRepository.findStoredBooksByMemberId(1L, PageRequest.of(page, 10));
+                bookListRepository.findStoredBooksByMemberId(1L, PageRequest.of(page - 1, 10));
 
         // Page<BookList>에서 content만 추출하여 변환
         return bookLists.getContent().stream()
@@ -143,7 +141,7 @@ public class BookListServiceImpl implements BookListService {
         BookList bookList =
                 bookListRepository
                         .findById(bookListId)
-                        .orElseThrow(() -> new GeneralException(ErrorStatus.BOOKLIST_NOT_FOUND));
+                        .orElseThrow(() -> new BookListHandler(ErrorStatus.BOOKLIST_NOT_FOUND));
 
         // 책 추가
         addBooksToBookList(bookList, request.getBooksId());
@@ -160,13 +158,13 @@ public class BookListServiceImpl implements BookListService {
         BookList bookList =
                 bookListRepository
                         .findById(bookListId)
-                        .orElseThrow(() -> new GeneralException(ErrorStatus.BOOKLIST_NOT_FOUND));
+                        .orElseThrow(() -> new BookListHandler(ErrorStatus.BOOKLIST_NOT_FOUND));
 
         // 책 리스트에서 책 찾기
         List<Book> books = bookRepository.findAllById(request.getBooksId());
 
         if (books.size() != request.getBooksId().size()) {
-            throw new GeneralException(ErrorStatus.BOOK_NOT_FOUND);
+            throw new BookListHandler(ErrorStatus.BOOK_NOT_FOUND);
         }
 
         // 기존에 존재하는 책을 확인
@@ -182,7 +180,7 @@ public class BookListServiceImpl implements BookListService {
                         .collect(Collectors.toList());
 
         if (duplicateBookIds.isEmpty()) {
-            throw new GeneralException(ErrorStatus.BOOKLIST_BOOK_NO_EXISTS);
+            throw new BookListHandler(ErrorStatus.BOOKLIST_BOOK_NO_EXISTS);
         }
 
         // 삭제할 책 엔티티를 BookListEntry에서 제거
@@ -214,7 +212,7 @@ public class BookListServiceImpl implements BookListService {
         List<Book> books = bookRepository.findAllById(bookIds);
 
         if (books.size() != bookIds.size()) {
-            throw new GeneralException(ErrorStatus.BOOK_NOT_FOUND);
+            throw new BookListHandler(ErrorStatus.BOOK_NOT_FOUND);
         }
 
         // 기존에 추가된 책을 확인
@@ -228,7 +226,7 @@ public class BookListServiceImpl implements BookListService {
                 bookIds.stream().filter(existingBookIds::contains).collect(Collectors.toList());
 
         if (!duplicateBookIds.isEmpty()) {
-            throw new GeneralException(ErrorStatus.BOOKLIST_BOOK_ALREADY_EXISTS);
+            throw new BookListHandler(ErrorStatus.BOOKLIST_BOOK_ALREADY_EXISTS);
         }
 
         // 새로 추가할 책 리스트에서 기존에 추가되지 않은 책만 선택
@@ -258,61 +256,4 @@ public class BookListServiceImpl implements BookListService {
         // BookList 업데이트
         bookListRepository.save(bookList);
     }
-
-    //    @Override
-    //    @Transactional
-    //    public void deleteBookInBookList(
-    //            Long bookListId, BookListRequestDTO.DeleteBookInBookListDTO request) {
-    //        // 책 리스트 찾기
-    //        BookList bookList =
-    //                bookListRepository
-    //                        .findById(bookListId)
-    //                        .orElseThrow(() -> new
-    // GeneralException(ErrorStatus.BOOKLIST_NOT_FOUND));
-    //
-    //        // 책 리스트에서 책 찾기
-    //        List<Book> books = bookRepository.findAllById(request.getBooksId());
-    //
-    //        if (books.size() != request.getBooksId().size()) {
-    //            throw new GeneralException(ErrorStatus.BOOK_NOT_FOUND);
-    //        }
-    //
-    //        // 기존에 존재하는 책을 확인
-    //        List<Long> existingBookIds =
-    //                bookList.getBookListEntry().stream()
-    //                        .map(entry -> entry.getBook().getId())
-    //                        .collect(Collectors.toList());
-    //
-    //        // 해당책 리스트에서 삭제하려는 아이디의 책이 있는지 확인
-    //        List<Long> duplicateBookIds =
-    //                request.getBooksId().stream()
-    //                        .filter(existingBookIds::contains)
-    //                        .collect(Collectors.toList());
-    //
-    //        if (duplicateBookIds.isEmpty()) {
-    //            throw new GeneralException(ErrorStatus.BOOKLIST_BOOK_NO_EXISTS);
-    //        }
-    //
-    //        // 삭제할 책 엔티티를 BookListEntry에서 제거
-    //        List<BookListEntry> entriesToRemove =
-    //                bookList.getBookListEntry().stream()
-    //                        .filter(entry -> duplicateBookIds.contains(entry.getBook().getId()))
-    //                        .collect(Collectors.toList());
-    //
-    //        // BookListEntry 삭제
-    //        bookList.getBookListEntry().removeAll(entriesToRemove);
-    //        entriesToRemove.forEach(entry -> bookListEntryRepository.delete(entry));
-    //
-    //        // 남은 책들의 number를 1부터 순서대로 재정렬
-    //        List<BookListEntry> remainingEntries = bookList.getBookListEntry();
-    //        for (int i = 0; i < remainingEntries.size(); i++) {
-    //            remainingEntries.get(i).setNumber(i + 1);
-    //        }
-    //
-    //        // BookList의 bookCnt 값 업데이트
-    //        bookList.setBookCnt(remainingEntries.size());
-    //
-    //        // BookList 업데이트
-    //        bookListRepository.save(bookList);
-    //    }
 }
