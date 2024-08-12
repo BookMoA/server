54fd373377f87b80932aa616837cce147f231356
2020147542 <tmfal5023@yonsei.ac.kr>
2024-08-12 23:29:54 +0900

    [refactor] 에러 코드 수정 및 s3 서비스 다시 연결

--- src/main/java/com/umc/server/service/MemberService/AwsServiceImpl.java
+++ src/main/java/com/umc/server/service/MemberService/AwsServiceImpl.java
@@ -1,20 +1,11 @@
 package com.umc.server.service.MemberService;
 
-import static com.umc.server.util.FileNameUtils.newFileName;
-
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.umc.server.apiPayload.code.status.ErrorStatus;
-import com.umc.server.apiPayload.exception.handler.MemberHandler;
 import com.umc.server.domain.Member;
 import com.umc.server.repository.MemberRepository;
+import com.umc.server.service.S3Service.S3Service;
 import com.umc.server.web.dto.response.MemberResponseDTO;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,11 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class AwsServiceImpl implements AwsService {
 
-    @Value("${S3_BUCKET}")
-    private String bucket;
-
-    private final AmazonS3Client amazonS3Client;
     private final MemberRepository memberRepository;
+    private final S3Service s3Service;
 
     // TODO: 프로필 정보 변경
     public MemberResponseDTO.EditProfileInfo editProfileInfo(
@@ -42,7 +30,7 @@ public class AwsServiceImpl implements AwsService {
         }
 
         if (file != null && !file.isEmpty()) {
-            final String newUrl = uploadFile(file);
+            final String newUrl = s3Service.uploadFile(file);
             signInMember.setProfileURL(newUrl);
         }
 
@@ -51,33 +39,5 @@ public class AwsServiceImpl implements AwsService {
                 updatedMember.getEmail(),
                 updatedMember.getNickname(),
                 updatedMember.getProfileURL());
-    }
-
-    // TODO: s3에 이미지 업로드
-    private String uploadFile(MultipartFile file) throws IOException {
-        if (file.getOriginalFilename() == null || file.getOriginalFilename().isEmpty()) {
-            throw new MemberHandler(ErrorStatus._BAD_REQUEST);
-        }
-
-        String fileName = newFileName(file.getOriginalFilename()); // s3에 저장될 이름
-        File convertedFile = convertMultiPartToFile(file); // 임시파일로 변경한 후 업로드
-
-        try {
-            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, convertedFile));
-
-            return amazonS3Client.getUrl(bucket, fileName).toString();
-
-        } finally {
-            convertedFile.delete(); // 임시 파일 삭제
-        }
-    }
-
-    // TODO: multipartFile -> 임시 File로 변경
-    private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
-        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
-            fos.write(file.getBytes());
-        }
-        return convertedFile;
     }
 }
