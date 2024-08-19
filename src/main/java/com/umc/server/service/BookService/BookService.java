5e7b1505b7c9094680562e1af1f0f035fb5d561d
jm <106303671+jinnieusLab@users.noreply.github.com>
2024-08-19 00:32:14 -0500

    [fix] 책 이미지 S3 변경 (#140)

--- src/main/java/com/umc/server/service/BookService/BookService.java
+++ src/main/java/com/umc/server/service/BookService/BookService.java
@@ -2,11 +2,15 @@ package com.umc.server.service.BookService;
 
 import com.umc.server.domain.Book;
 import com.umc.server.web.dto.request.BookRequestDTO;
+import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface BookService {
-    Book createBook(BookRequestDTO.CreateBookDTO createBookDTO);
+    Book createBook(BookRequestDTO.CreateBookDTO createBookDTO, MultipartFile imgUrl)
+            throws IOException;
 
     Book readBook(Long bookId);
 
-    Book updateBook(Long bookId, BookRequestDTO.UpdateBookDTO updateBookDTO);
+    Book updateBook(Long bookId, BookRequestDTO.UpdateBookDTO updateBookDTO, MultipartFile imgUrl)
+            throws IOException;
 }
--- src/main/java/com/umc/server/web/controller/BookRestController.java
+++ src/main/java/com/umc/server/web/controller/BookRestController.java
@@ -12,8 +12,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,10 +35,12 @@ public class BookRestController {
                 description = "잘못된 요청입니다.",
                 content = @Content(schema = @Schema(implementation = ApiResponse.class)))
     })
-    @PostMapping
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ApiResponse<BookResponseDTO.CreateBookResultDTO> createBook(
-            @RequestBody @Valid BookRequestDTO.CreateBookDTO createBookDTO) {
-        Book book = bookService.createBook(createBookDTO);
+            @RequestBody @Valid BookRequestDTO.CreateBookDTO createBookDTO,
+            @RequestPart(value = "imgUrl", required = false) MultipartFile imgUrl)
+            throws IOException {
+        Book book = bookService.createBook(createBookDTO, imgUrl);
         return ApiResponse.onSuccess(BookConverter.toCreateBookResultDTO(book));
     }
 
@@ -68,11 +73,15 @@ public class BookRestController {
                 content = @Content(schema = @Schema(implementation = ApiResponse.class)))
     })
     @Parameter(name = "bookId", description = "책의 아이디, path variable 입니다.")
-    @PatchMapping("/{bookId}")
+    @PatchMapping(
+            value = "/{bookId}",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ApiResponse<BookResponseDTO.BookPreviewDTO> updateBook(
             @PathVariable(name = "bookId") Long bookId,
-            @RequestBody @Valid BookRequestDTO.UpdateBookDTO updateBookDTO) {
-        Book book = bookService.updateBook(bookId, updateBookDTO);
+            @RequestBody @Valid BookRequestDTO.UpdateBookDTO updateBookDTO,
+            @RequestPart(value = "imgUrl", required = false) MultipartFile imgUrl)
+            throws IOException {
+        Book book = bookService.updateBook(bookId, updateBookDTO, imgUrl);
         return ApiResponse.onSuccess(BookConverter.toBookPreviewDTO(book));
     }
 }
