e675c39ff28649725a115f7283124cfc60c77399
Dayun Lee <wohd7877@naver.com>
2024-08-23 01:59:36 +0900

    [fix] 책리스트 보관함 조회 오류 수정

--- src/main/java/com/umc/server/repository/BookListRepository.java
+++ src/main/java/com/umc/server/repository/BookListRepository.java
@@ -11,7 +11,7 @@ public interface BookListRepository extends JpaRepository<BookList, Long> {
     // 멤버 ID와 저장된 책 리스트를 페이지네이션과 함께 조회 -> 보관함
     @Query(
             "SELECT bl FROM BookList bl WHERE bl.member.id = :memberId "
-                    + "OR EXISTS (SELECT 1 FROM MemberBookList mbl WHERE mbl.bookList.id = bl.id AND mbl.isStored = true)")
+                    + "OR EXISTS (SELECT 1 FROM MemberBookList mbl WHERE mbl.bookList.id = bl.id AND mbl.isStored = true AND mbl.member.id = :memberId)")
     Page<BookList> findStoredBooksByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
     @Query("SELECT b FROM BookList b WHERE b.title LIKE %:title% AND b.listStatus = 'PUBLIC'")
